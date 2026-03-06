bb2e5024221cba7e1fa3c5c2848acc166ce3c23c
Jaime Galindo <jaime.galindo@iteso.mx>
2026-03-05 21:16:41 -0600

    servicio de node.js funcionando con la ruta health para inicio de servicio

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Endpoint de salud básico
+app.get('/health', (req, res) => res.json({ status: 'UP', service: 'Orders' }));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor de Órdenes (Node 22) corriendo en puerto ${PORT}`);
+});
