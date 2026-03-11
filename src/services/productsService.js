d93e5e646d4eb8d60b58f9e8fb263ba96f369c86
Jaime Galindo <jaime.galindo@iteso.mx>
2026-03-10 20:41:22 -0600

    productsService conectado y haciendo requests al microservicio de productos

--- src/services/productsService.js
+++ src/services/productsService.js
@@ -3,6 +3,8 @@
  * Cliente para el microservicio de Productos.
  */
 
+const { response } = require("express");
+
 // Mock de inventario para que puedas probar hoy mismo
 const fakeInventory = {
     "P001": {
@@ -44,16 +46,52 @@ const fakeInventory = {
 };
 
 const getProductById = async (productId) => {
+    const PRODUCTS_SERVICE_URL = process.env.PRODUCTS_SERVICE_URL;
+    const finalUrl = PRODUCTS_SERVICE_URL + '/products/' + productId;
+
     console.log(`[Products Service] Consultando producto: ${productId}`);
-    // Simula una latencia de red
-    return new Promise((resolve) => {
-        setTimeout(() => resolve(fakeInventory[productId] || null), 100);
-    });
+
+    const response = await fetch(finalUrl);
+
+    if (response.status === 404) {
+        return null;
+    } else if (!response.ok) {
+        throw new Error(`Error en Products Service: ${response.statusText}`);
+    } 
+    
+    return await response.json();
 };
 
 const updateProductStock = async (productId, quantity) => {
     console.log(`[Products Service] Simulando reducción de stock para ${productId}: -${quantity}`);
-    return { success: true };
+    const PRODUCTS_SERVICE_URL = process.env.PRODUCTS_SERVICE_URL;
+    const finalUrl = PRODUCTS_SERVICE_URL + '/products/' + productId + '/stock';
+
+    const product = await getProductById(productId);
+
+    if (!product) {
+        return null;
+    } else {
+        const newStock = product.stock - quantity;
+        const response = await fetch(finalUrl, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                stock: newStock
+            })
+        });
+
+        if (!response.ok) {
+            const errorData = response.json();
+            throw new Error(`Error al actualizar stock: ${errorData.message || response.statusText}`);            
+        }
+
+        const data = await response.json();
+
+        return { success: true, data};
+    }
 };
 
 module.exports = {
